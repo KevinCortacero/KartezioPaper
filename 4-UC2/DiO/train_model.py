7f8491b5f87d9d4bba34d208fc7fe4987f3bd559
Kévin Cortacero <kevin.cortacero@inserm.fr>
2024-04-21 22:23:58 +0200

    added missing files

---
+++ 4-UC2/DiO/train_model.py
@@ -0,0 +1,26 @@
+from kartezio.apps.instance_segmentation import create_instance_segmentation_model
+from kartezio.endpoint import LocalMaxWatershed
+from kartezio.dataset import read_dataset
+from kartezio.training import train_model
+from kartezio.preprocessing import SelectChannels
+
+
+CHANNEL_DiO = 1
+preprocessing = SelectChannels([CHANNEL_DiO])
+DATASET = "./dataset"
+OUTPUT = "./new_trained_models"
+
+
+if __name__ == "__main__":
+    generations = 100
+    _lambda = 5
+    frequency = 5
+    model = create_instance_segmentation_model(
+        generations,
+        _lambda,
+        inputs=1,
+        outputs=1,
+        endpoint=LocalMaxWatershed(threshold=1, markers_distance=5),
+    )
+    dataset = read_dataset(DATASET)
+    elite, _ = train_model(model, dataset, OUTPUT, preprocessing=preprocessing, callback_frequency=frequency)
